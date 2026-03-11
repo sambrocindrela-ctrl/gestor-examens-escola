fc61c8381eb7c27a195a9df5d22fa88ab876a515
sambrocindrela-ctrl <sambrocindrela@gmail.com>
2026-03-11 09:27:09 +0100

    Update PlannerToolbar.vue

--- src/components/PlannerToolbar.vue
+++ src/components/PlannerToolbar.vue
@@ -249,6 +249,10 @@ function handleImportExcel(event: Event) {
             Exportar calendari en Excel
           </button>
 
+          <button @click="emit('export-gef-excel')" class="px-3 py-2 border rounded-xl shadow-sm bg-white hover:bg-gray-50">
+           Exportar Excel GEF
+          </button>
+
           <button @click="emit('export-word')" class="px-3 py-2 border rounded-xl shadow-sm bg-white hover:bg-gray-50">
             Exportar calendari en Word
           </button>
