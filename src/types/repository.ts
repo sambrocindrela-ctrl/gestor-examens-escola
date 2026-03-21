6dee30c493efe1cf8b501cf02665cd4bf7997439
sambrocindrela-ctrl <sambrocindrela@gmail.com>
2026-03-21 18:06:31 +0100

    Create repository.ts

---
+++ src/types/repository.ts
@@ -0,0 +1,24 @@
+import type { ExamPlannerDocument } from "./examPlanner";
+import type { CalendarSummary, SavedCalendar } from "./savedCalendar";
+
+export interface CreateCalendarInput {
+  name: string;
+  academicYear?: string;
+  document: ExamPlannerDocument;
+}
+
+export interface UpdateCalendarInput {
+  id: string;
+  name?: string;
+  academicYear?: string;
+  document: ExamPlannerDocument;
+}
+
+export interface CalendarRepository {
+  listCalendars(): Promise<CalendarSummary[]>;
+  getCalendar(id: string): Promise<SavedCalendar>;
+  createCalendar(input: CreateCalendarInput): Promise<SavedCalendar>;
+  updateCalendar(input: UpdateCalendarInput): Promise<SavedCalendar>;
+  deleteCalendar(id: string): Promise<void>;
+  duplicateCalendar(id: string, newName: string): Promise<SavedCalendar>;
+}
