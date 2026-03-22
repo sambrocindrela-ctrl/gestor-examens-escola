ea146de834573743bdaca9c2ada1f20a71661c5f
sambrocindrela-ctrl <sambrocindrela@gmail.com>
2026-03-22 23:09:04 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -616,6 +616,13 @@ async function handleDeleteSelectedSupabaseCalendar() {
   </button>
 
   <button
+  class="px-4 py-2 rounded bg-amber-600 text-white hover:bg-amber-700"
+  @click="handleRenameSelectedSupabaseCalendar"
+>
+  Reanomenar seleccionat
+</button>
+
+  <button
   class="px-4 py-2 rounded bg-red-600 text-white hover:bg-red-700"
   @click="handleDeleteSelectedSupabaseCalendar"
 >
